a63b15049bc7126df0a9d213577336a8d4254e4c
Vincent Pelletier <plr.vincent@gmail.com>
2017-11-17 12:20:40 +0000

    Trivial test.

---
+++ tests.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+from __future__ import absolute_import
+import tempfile
+import libaio
+
+def main():
+    temp = tempfile.TemporaryFile()
+    temp.write('blah')
+    with libaio.AIOContext(1) as io_context:
+        read_block = libaio.AIOBlock(
+            libaio.AIOBLOCK_MODE_READ,
+            temp,
+            [
+                bytearray(2),
+                bytearray(2),
+            ],
+            0,
+        )
+        write_block = libaio.AIOBlock(
+            libaio.AIOBLOCK_MODE_WRITE,
+            temp,
+            [
+                bytearray("u"),
+                bytearray("e"),
+            ],
+            2,
+        )
+        print read_block, write_block
+        io_context.submit([read_block])
+        for event in io_context.getEvents():
+            print event
+        temp.seek(0)
+        print temp.read()
+        io_context.submit([write_block])
+        for event in io_context.getEvents():
+            print event
+        temp.seek(0)
+        print temp.read()
+
+if __name__ == '__main__':
+    main()
