77c7eb5bab3d0e34a472f30432278c175591453a
Vincent Pelletier <plr.vincent@gmail.com>
2024-02-10 12:58:01 +0000

    libaio.eventfd: Unhardcode EFD_CLOEXEC and EFD_NONBLOCK definitions

--- libaio/eventfd.py
+++ libaio/eventfd.py
@@ -17,6 +17,7 @@
 Minimal adaptation of sys/eventfd.h and bits/eventfs.h .
 """
 from ctypes import CDLL, c_uint, c_int, get_errno
+import os
 
 libc = CDLL("libc.so.6", use_errno=True)
 # pylint: disable=unused-argument
@@ -31,6 +32,10 @@ eventfd.restype = c_int
 eventfd.argtypes = (c_int, c_uint)
 eventfd.errcheck = _raise_errno_on_neg_one
 
-EFD_CLOEXEC = 0o02000000
-EFD_NONBLOCK = 0o00004000
 EFD_SEMAPHORE = 0o00000001
+# Note: the glibc hardcodes those EFD_ constants, duplicating their O_
+# counterpart. The kernel equivalent of that header, and Android's libc
+# headers, does instead refers to the latter when declaring the former.
+# Follow the latter group, as it looks cleaner.
+EFD_CLOEXEC = os.O_CLOEXEC
+EFD_NONBLOCK = os.O_NONBLOCK
